929cf41f906cdd4c235403a72b293e83464efc8e
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2021-04-13 01:29:36 +0500

    fixed minor issue

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -11,7 +11,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Signup = () => {
+const Signup = ({ setMessage }) => {
   const { push } = useRouter();
   const classes = useStyles();
 
